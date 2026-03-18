2eee9eb1dbaedadb561e4ced177ab82b321ee179
Swetang Pandit <swetangpandit@gmail.com>
2026-03-18 14:53:47 +1100

    Arithmetic Expression.

---
+++ Arithmetic_Expressions/Arithmetic_Expressions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Arithmetic_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 10 + 100/3;
+            Console.WriteLine("The result of the expression is: " + result);
+        }
+    }
+}
